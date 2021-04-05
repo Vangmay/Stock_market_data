1d102fedf6f70da10c08825a44ba97c800133141
Vangmay <78148958+Vangmay@users.noreply.github.com>
2021-04-05 13:49:40 +0530

    Update stock_finplot.py

--- stock_finplot.py
+++ stock_finplot.py
@@ -1,29 +1,45 @@
 import finplot as fplt
 import yfinance as yf
 from tkinter import *
+import re
+from tkinter import messagebox 
+from tkinter import Menu
 root = Tk()
 root.geometry('800x500')
 #ENTRY BOXES
-Ticker_entry = Entry(root)
+Ticker_entry = Entry(root,width = 30)
 Ticker_entry.pack()
 Ticker_entry.insert(0,'Enter ticker')
-Start_entry = Entry(root)
+Start_entry = Entry(root,width = 30)
 Start_entry.pack()
-Start_entry.insert(0,'Enter Start date')
-End_entry = Entry(root)
+Start_entry.insert(0,'Enter Start date(yyyy-mm-dd)')
+End_entry = Entry(root,width = 30)
 End_entry.pack()
-End_entry.insert(0,'Enter End date')
-Period_entry = Entry(root)
-Period_entry.pack()
-Period_entry.insert(0,'Enter Period')
-Interval_entry = Entry(root)
+End_entry.insert(0,'Enter End date(yyyy-mm-dd)')
+Interval_entry = Entry(root,width = 30)
 Interval_entry.pack()
 Interval_entry.insert(0,'Enter Interval')
 #ENTRY BOXES
+#error box
+menuBar = Menu(root)
+root.config(menu=menuBar)
+def _msgBox(): 
+    messagebox.showerror("Error",'Please enter correct format of date in yy-mm-dd')
+    infoMenu=Menu(menuBar,tearoff=0)
+    infoMenu.add_command(command = _msgBox)
+
 def input_ticker():
-    stats = yf.download(tickers=Ticker_entry.get(),start = Start_entry.get(),end = End_entry.get(),period = Period_entry.get(),interval=Interval_entry.get())
-    fplt.candlestick_ochl(stats[['Open','High','Low','Close']])
-    fplt.show()
+    pattern = r'\d{4}-\d{2}-\d{2}'
+    match_start = re.match(pattern,Start_entry.get())
+    match_end = re.match(pattern,End_entry.get())
+    match_start_result = bool(match_start)
+    match_end_result = bool(match_end)
+    if match_start_result == False or match_end_result == False:
+        _msgBox()
+    else:
+        stats = yf.download(tickers=Ticker_entry.get(),start = Start_entry.get(),end = End_entry.get(),interval=Interval_entry.get())
+        fplt.candlestick_ochl(stats[['Open','High','Low','Close']])
+        fplt.show()
 #making button
 btn = Button(root,text = 'submit',command=input_ticker)
 btn.pack()
